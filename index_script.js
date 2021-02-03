a9bbff6a405dde3dd15e420b95fc31396b96b025
Tony Wang <tonywang0321@gmail.com>
2021-02-03 00:17:32 -0500

    animate test

--- index_script.js
+++ index_script.js
@@ -1,9 +1,17 @@
 document.getElementById("myImage").innerHTML = "Hello Spongebob!";
-document.getElementById("myImage").animate([
-    // keyframes
-    { transform: 'rotate(360deg)' }
-  ], {
-    // timing options
-    duration: 1000,
+
+var SpongeTumbling = [
+    { transform: 'rotate(0) translate3D(-50%, -50%, 0)'}, 
+    { transform: 'rotate(360deg) translate3D(-50%, -50%, 0)'}
+  ];
+  
+  var SpongeTiming = {
+    duration: 3000,
     iterations: Infinity
-  });
+  }
+
+
+document.getElementById("myImage").animate(
+    SpongeTumbling,
+    SpongeTiming
+)
